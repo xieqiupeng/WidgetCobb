c4ae348b872d5d4d2c6c1b56ca878fd77f07e279
xieqiupeng <xieqiupeng@gmail.com>
2018-08-23 11:00:51 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### WidgetCobol
 自己用的一些常用的控件和工具。
 ```
-com.github.xieqiupeng:lib-cobb:latest
+implementation "com.github.xieqiupeng:WidgetCobb:v1.8.2"
 ```
--- lib-cobb/src/main/java/com/twirling/lib_cobb/adapter/DataBindingAdapter.java
+++ lib-cobb/src/main/java/com/twirling/lib_cobb/adapter/DataBindingAdapter.java
@@ -4,20 +4,27 @@ import android.databinding.BindingAdapter;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 /**
  * Target: 为databinding补充绑定方法
  */
 public class DataBindingAdapter {
-	@BindingAdapter("stagePhoto")
-	public static void setStagePhotoByPath(ImageView view,
-	                                       String videoPath) {
-		Glide.with(view.getContext())
-				.load(videoPath)
-				.asBitmap()
-				.thumbnail(0.1f)
-				.centerCrop()
+    @BindingAdapter("stagePhoto")
+    public static void setStagePhotoByPath(ImageView view,
+                                           String videoPath) {
+        RequestOptions options = new RequestOptions()
+//				.diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                .centerCrop()
 //				.placeholder(drawable)
-				.into(view);
-	}
+                ;
+        //
+        Glide.with(view.getContext())
+                .asBitmap()
+                .load(videoPath)
+                .thumbnail(0.1f)
+                .into(view)
+        ;
+    }
 }
